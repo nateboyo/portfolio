af6d2c3b0414b4a1ac74e11d7a659a7a768848e0
Nathan Garrovillas <garrovillasnathan@gmail.com>
2026-05-13 01:18:50 -0400

    feat: /projects route wired with PageFrame + ProjectSwitcher

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,10 @@
+import PageFrame from '@/components/PageFrame';
+import ProjectSwitcher from '@/components/ProjectSwitcher';
+
+export default function ProjectsPage() {
+  return (
+    <PageFrame title="Projects">
+      <ProjectSwitcher />
+    </PageFrame>
+  );
+}
